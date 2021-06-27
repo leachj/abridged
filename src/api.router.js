c70bf69204abca21d17f3fda1eda9d0cda485ef3
Jonathan Leach <mail@elletwo.co.uk>
2021-06-27 12:27:19 +0100

    added some error handling to the API

--- src/api.router.js
+++ src/api.router.js
@@ -1,7 +1,24 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const validUrl = require('valid-url');
+
+
+// we wouldnt want some codes to be created for security reasons.
+// this list is not exhaustive!
+const bannedCodes = [
+  'api',
+  'robots.txt'
+]
 
 router.post('/links', (req, res) => {
+
+  if(!req.body.code || bannedCodes.includes(req.body.code)) {
+    res.status(400).send({error: 'invalid code'})
+  }
+  if(!req.body.target || !validUrl.isUri(req.body.target)) {
+    res.status(400).send({error: 'invalid target'})
+  }
+
   req.app.links[req.body.code] = req.body
   res.send(req.body)
 })
--- tests/api.test.js
+++ tests/api.test.js
@@ -20,4 +20,48 @@ describe('api to manage links', () => {
 
   });
 
+  it('Should not be able to create a link to a banned code', async () => {
+
+    const link = {code: 'api', target: 'http://example.com'}
+    const response = await request(abridged).post('/api/v1/links').send(link).set('Accept', 'application/json')
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('invalid code')
+
+    expect(abridged.links[link.code]).toBeUndefined
+
+  });
+
+  it('Should not be able to create a link without a code', async () => {
+
+    const link = {target: 'http://example.com'}
+    const response = await request(abridged).post('/api/v1/links').send(link).set('Accept', 'application/json')
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('invalid code')
+
+    expect(abridged.links[link.code]).toBeUndefined
+
+  });
+
+  it('Should not be able to create a link to an invalid target', async () => {
+
+    const link = {code: 'example', target: 'not-a-url'}
+    const response = await request(abridged).post('/api/v1/links').send(link).set('Accept', 'application/json')
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('invalid target')
+
+    expect(abridged.links[link.code]).toBeUndefined
+
+  });
+
+  it('Should not be able to create a link without a target', async () => {
+
+    const link = {code: 'example'}
+    const response = await request(abridged).post('/api/v1/links').send(link).set('Accept', 'application/json')
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('invalid target')
+
+    expect(abridged.links[link.code]).toBeUndefined
+
+  });
+
 });
